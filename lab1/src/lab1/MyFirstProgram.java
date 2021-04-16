36dbd5accd91745abb96126148ee2681a90fc057
pratpittman12 <49230581+pratpittman12@users.noreply.github.com>
2021-04-16 15:26:00 +0930

    Add files via upload

---
+++ lab1/src/lab1/MyFirstProgram.java
@@ -0,0 +1,25 @@
+package lab1;
+
+public class MyFirstProgram {
+	public static void main(String[] args) 
+	 { 
+	 System.out.println("My Java Program Running!"); 
+	 int a = 5; 
+	 int b = 8; 
+	 float f = 8.0f; 
+	 boolean c = true; 
+	 boolean d = false; 
+	 System.out.println("a+b*f " + (a+b*f));
+	 System.out.println("b value is " + b); 
+	 System.out.println("b/2 value is " + b/2); 
+	 b++; 
+	 System.out.println("b value is " + b); 
+	 System.out.println("b/2 value is " + b/2); 
+	 System.out.println("f value is " + f); 
+	 System.out.println("f/2 value is " + f/2); 
+	 f++; 
+	 System.out.println("f value is " + f); 
+	 System.out.println("f/2 value is " + f/2); 
+	 
+	 }
+}
